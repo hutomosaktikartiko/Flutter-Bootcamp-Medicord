36e3852bb5529994b0b4621be6002750fff5470d
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-09-07 21:34:20 +0700

    Day 3

--- lib/day_3/screens/home_day_3_screen.dart
+++ lib/day_3/screens/home_day_3_screen.dart
@@ -3,8 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../cubit/list_posts_cubit.dart';
 
-class HomeDay3Screen extends StatelessWidget {
+class HomeDay3Screen extends StatefulWidget {
   const HomeDay3Screen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeDay3Screen> createState() => _HomeDay3ScreenState();
+}
+
+class _HomeDay3ScreenState extends State<HomeDay3Screen> {
+  Future<void> _onRefresh() async {
+    context.read<ListPostsCubit>().getListPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,40 +22,43 @@ class HomeDay3Screen extends StatelessWidget {
         title: const Text("GET POSTS"),
         centerTitle: true,
       ),
-      body: BlocBuilder<ListPostsCubit, ListPostsState>(
-        builder: (context, state) {
-          if (state is ListPostsLoaded) {
-            return SingleChildScrollView(
-              child: Column(
-                children: state.posts
-                    .asMap()
-                    .map(
-                      (key, value) => MapEntry(
-                        key,
-                        ListTile(
-                          title: Text(value.title ?? "-"),
-                          subtitle: Text(value.body ?? "-"),
+      body: RefreshIndicator(
+        onRefresh: _onRefresh,
+        child: BlocBuilder<ListPostsCubit, ListPostsState>(
+          builder: (context, state) {
+            if (state is ListPostsLoaded) {
+              return SingleChildScrollView(
+                child: Column(
+                  children: state.posts
+                      .asMap()
+                      .map(
+                        (key, value) => MapEntry(
+                          key,
+                          ListTile(
+                            title: Text(value.title ?? "-"),
+                            subtitle: Text(value.body ?? "-"),
+                          ),
                         ),
-                      ),
-                    )
-                    .values
-                    .toList(),
-              ),
-            );
-          } else if (state is ListPostsEmpty) {
+                      )
+                      .values
+                      .toList(),
+                ),
+              );
+            } else if (state is ListPostsEmpty) {
+              return const Center(
+                child: Text("List Posts is Empty"),
+              );
+            } else if (state is ListPostsError) {
+              return Center(
+                child: Text(state.message ?? "-"),
+              );
+            }
+      
             return const Center(
-              child: Text("List Posts is Empty"),
+              child: CircularProgressIndicator(),
             );
-          } else if (state is ListPostsError) {
-            return Center(
-              child: Text(state.message ?? "-"),
-            );
-          }
-
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: const MaterialApp(
-        home: HomeDay4Screen(),
+        home: HomeDay3Screen(),
       ),
     );
   }
